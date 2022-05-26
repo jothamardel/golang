b419a08062789db021e13c964e5c8cf5281da353
jothamardel <ardelmbiplang@gmail.com>
2022-05-27 00:03:26 +0100

    refactoring exercise

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -11,18 +11,20 @@ package main
 import "fmt"
 
 func main() {
-	var number int
-	for i := 1; i <= 100; i++ {
-		number = i
-		switch i := number; {
+
+	for i := 1; i <= 50; i++ {
+		divisibleBy3 := i%3 == 0
+		divisibleBy5 := i%5 == 0
+
+		switch i := i; {
 		//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
-		case (i%3 == 0) && (i%5 == 0):
+		case divisibleBy3 && divisibleBy5:
 			fmt.Println("FizzBuzz")
 			//  - Print "Fizz" if the integer is divisible by 3
-		case i%3 == 0:
+		case divisibleBy3:
 			fmt.Println("Fizz")
 			//  - Print "Buzz" if the integer is divisible by 5
-		case i%5 == 0:
+		case divisibleBy5:
 			fmt.Println("Buzz")
 			//* Print integers 1 to 50, except:
 		default:
