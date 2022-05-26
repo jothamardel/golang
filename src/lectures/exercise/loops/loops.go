1c3d6e8bbba4e86e064fc9ccb55658fc8a17b051
jothamardel <ardelmbiplang@gmail.com>
2022-05-27 00:05:36 +0100

    implementing fizzbuzz using if else statement

--- src/lectures/exercise/loops/loops.go
+++ src/lectures/exercise/loops/loops.go
@@ -16,18 +16,28 @@ func main() {
 		divisibleBy3 := i%3 == 0
 		divisibleBy5 := i%5 == 0
 
-		switch i := i; {
-		//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
-		case divisibleBy3 && divisibleBy5:
+		// switch i := i; {
+		// //  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+		// case divisibleBy3 && divisibleBy5:
+		// 	fmt.Println("FizzBuzz")
+		// 	//  - Print "Fizz" if the integer is divisible by 3
+		// case divisibleBy3:
+		// 	fmt.Println("Fizz")
+		// 	//  - Print "Buzz" if the integer is divisible by 5
+		// case divisibleBy5:
+		// 	fmt.Println("Buzz")
+		// 	//* Print integers 1 to 50, except:
+		// default:
+		// 	fmt.Println(i)
+		// }
+
+		if divisibleBy3 && divisibleBy5 {
 			fmt.Println("FizzBuzz")
-			//  - Print "Fizz" if the integer is divisible by 3
-		case divisibleBy3:
+		} else if divisibleBy3 {
 			fmt.Println("Fizz")
-			//  - Print "Buzz" if the integer is divisible by 5
-		case divisibleBy5:
+		} else if divisibleBy5 {
 			fmt.Println("Buzz")
-			//* Print integers 1 to 50, except:
-		default:
+		} else {
 			fmt.Println(i)
 		}
 	}
